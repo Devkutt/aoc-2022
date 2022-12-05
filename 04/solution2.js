e64b6efde09b4fc128a2a570718d18e40de67640
Kevin Schäfer <kevin.schaefer@innoq.com>
2022-12-05 08:59:41 +0100

    day 4

---
+++ 04/solution2.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const lines = fs.readFileSync('input.txt', 'utf-8').split('\n').filter(line => line != "");
+
+const ranges = lines.map(line => line.split(',').map(part => part.split('-').map(num => parseInt(num))));
+
+const overlapping = ranges.filter(pair => {
+    return (
+        (pair[0][0] <= pair[1][0] && pair[0][1] >= pair[1][1] || pair[1][0] <= pair[0][0] && pair[1][1] >= pair[0][1])
+         || (pair[0][0] <= pair[1][0] && pair[0][1] >= pair[1][0] || pair[0][0] <= pair[1][1] && pair[0][1] >= pair[1][1])
+         || (pair[1][0] <= pair[0][0] && pair[1][1] >= pair[0][0] || pair[1][0] <= pair[0][1] && pair[1][1] >= pair[0][1])
+    )
+});
+
+console.log(overlapping.length);
